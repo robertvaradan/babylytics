83c3c1030b7b2a0cf473e56098fc1bcfd3f13cab
robertvaradan <robertvaradan@gmail.com>
2024-12-31 23:10:23 -0500

    Add PWA manifest

---
+++ app/manifest.ts
@@ -0,0 +1,10 @@
+import { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        short_name: 'Babylytics',
+        name: 'Babylytics',
+        display: 'standalone',
+        start_url: '/baby',
+    }
+}
